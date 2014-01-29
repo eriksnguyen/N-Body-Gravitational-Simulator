b5dd9152e0b853b3f28435ddd1e1635640de6e3e
Michael Sun <micsun@mit.edu>
2014-01-29 17:05:28 -0500

    nope changed back to force

--- include/nbody/body.h
+++ include/nbody/body.h
@@ -9,7 +9,7 @@ namespace nbody {
   class Body {
     Vector3f _position;
     Vector3f _velocity;
-    Vector3f _acceleration;
+    Vector3f _force;
     float _mass;
   public:
     Body() : _position{}, _velocity{}, _force{}, _mass{} {}
@@ -17,8 +17,8 @@ namespace nbody {
     inline Vector3f& position() { return _position; }
     inline Vector3f velocity() const { return _velocity; }
     inline Vector3f& velocity() { return _velocity; }
-    inline Vector3f force() const { return _acceleration; }
-    inline Vector3f& force() { return _acceleration; }
+    inline Vector3f force() const { return _force; }
+    inline Vector3f& force() { return _force; }
     inline float mass() const { return _mass; }
     friend std::istream& operator>>( std::istream &is, Body &body );
     friend std::ostream& operator<<( std::ostream &os, const Body &body );
