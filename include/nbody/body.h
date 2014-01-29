949f0d1be5432ad48c1f4ada167887d90e7eca09
Michael Sun <micsun@mit.edu>
2014-01-29 13:52:26 -0500

    Updated body header with double precision

--- include/nbody/body.h
+++ include/nbody/body.h
@@ -3,14 +3,21 @@
 
 namespace nbody{
 	class Body{
-		Vector3d _position;
+		Vector3d _position; // use double > float; will store doubles and print floats
 		Vector3d _velocity;
 		Vector3d _acceleration;
 		double _mass;
 	public:
-		Body{
-			
-		}
+		Body(): _position{}, _velocity{}, _acceleration{}, _mass{} {}
+		inline Vector3d position() const { return _position; }
+		inline Vector3d& position() { return _position; }
+		inline Vector3d velocity() const { return _velocity; }
+		inline Vector3d& velocity() { return _velocity; }
+		inline Vector3d acceleration() const { return _acceleration; }
+		inline Vector3d& acceleration() { return _acceleration; }
+		inline float mass() const { return _mass; }
+		friend std::istream& operator>>( std::istream &is, Body &body );
+		friend std::ostream& operator<<( std::ostream &os, const Body &body );
 	};
 
 } // namespace nbody
