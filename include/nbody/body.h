53f35d04a5652f08d82af0352744ecbbbf9bf1fb
Erik Nguyen <eriksnguyen@gmail.com>
2014-01-29 16:32:53 -0500

    Added given source and header files from class

--- include/nbody/body.h
+++ include/nbody/body.h
@@ -1,25 +1,30 @@
 #ifndef _NBODY_BODY_H
 #define _NBODY_BODY_H
 
-namespace nbody{
-	class Body{
-		Vector3d _position; // use double > float; will store doubles and print floats
-		Vector3d _velocity;
-		Vector3d _acceleration;
-		double _mass;
-	public:
-		Body(): _position{}, _velocity{}, _acceleration{}, _mass{} {}
-		inline Vector3d position() const { return _position; }
-		inline Vector3d& position() { return _position; }
-		inline Vector3d velocity() const { return _velocity; }
-		inline Vector3d& velocity() { return _velocity; }
-		inline Vector3d acceleration() const { return _acceleration; }
-		inline Vector3d& acceleration() { return _acceleration; }
-		inline float mass() const { return _mass; }
-		friend std::istream& operator>>( std::istream &is, Body &body );
-		friend std::ostream& operator<<( std::ostream &os, const Body &body );
-	};
+#include <nbody/Vector3.h>
+
+#include <iosfwd>
+
+namespace nbody {
+
+  class Body {
+    Vector3f _position;
+    Vector3f _velocity;
+    Vector3f _force;
+    float _mass;
+  public:
+    Body() : _position{}, _velocity{}, _force{}, _mass{} {}
+    inline Vector3f position() const { return _position; }
+    inline Vector3f& position() { return _position; }
+    inline Vector3f velocity() const { return _velocity; }
+    inline Vector3f& velocity() { return _velocity; }
+    inline Vector3f force() const { return _force; }
+    inline Vector3f& force() { return _force; }
+    inline float mass() const { return _mass; }
+    friend std::istream& operator>>( std::istream &is, Body &body );
+    friend std::ostream& operator<<( std::ostream &os, const Body &body );
+  };
 
 } // namespace nbody
 
-#endif //_NBODY_BODY_H
+#endif // _NBODY_BODY_H
