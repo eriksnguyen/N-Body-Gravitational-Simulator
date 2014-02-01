7c0d14d3859cfc640d3d0863c373223701181043
Michael Z Sun <micsun@mit.edu>
2014-02-01 15:16:55 -0500

    hi

--- src/RungeKutta.cpp
+++ src/RungeKutta.cpp
@@ -4,12 +4,21 @@
 
 namespace nbody{
 
-	void updateBody( Body& body, float dt, float dampingFactor ){
-		Vector3f r, v, a, k1, k2, k3, k4;
-
-		r = body.position();
-		v = body.velocity();
-		a = body.accel();
+	void updateBody( Body& body0, float dt, float dampingFactor ){
+	  Vector3f r0, v0, a0, k1, k2, k3, k4;
+	  /* Body BodyArray[5];
+	  BodyArray[0] = body0;
+	  float kv[4];
+	  float kr[4];
+		r0 = body0.position();
+		v0 = body0.velocity();
+		a0 = body0.accel();
+		kv[0] = a0;
+		kr[0] = v0;
+		for ( int i = 1; i < 5; ++i){
+		  BodyArray[i].velocity() = BodyArray[0].velocity() + kv[0]*dt;
+		  
+		  }*/
 		k1 = (v + (a * dt))*dt;
 		k2 = (v + 0.5f*k1 + (a * 1.5f * dt))*dt;
 		k3 = (v + 0.5f*k2 + (a * 2.0f * dt))*dt;
@@ -17,7 +26,7 @@ namespace nbody{
 
 		v = v + (a * dt);
 		v = v * dampingFactor;
-		r = r + 1.0f/6.0f*( k1 + 2.0f*k2 + 2.0f*k3 + k4 );
+		r = r + 1.0f/6.0f*( k1 + 2.0f*k2 + 2.0f*k3 + k4 );*/
 
 		body.position() = r;
 		body.velocity() = v;
