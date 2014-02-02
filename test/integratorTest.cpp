ff64855091ca5572b2c763afc2b0bd81d8f30da2
Cyndia Cao <cyndiac@mit.edu>
2014-02-02 16:43:59 -0500

    mwahahhahahaha its not actually cyndia jsalgjsdalkfjasdgjals i can write whatever i want here potatoes!

--- test/integratorTest.cpp
+++ test/integratorTest.cpp
@@ -45,6 +45,45 @@ TEST( integratorTest, eulerIntegration1 ){
 	
 }
 
+TEST( integratorTest, eulerIntegration2 ){
+	auto integrationScheme = Euler{};
+	auto body = Body{};
+	//Initial conditions
+	body.position() = Vector3f{ 1, 0, 0 };
+	body.velocity() = Vector3f{ 1, 0, 0 };
+	body.accel() = Vector3f{ 1, 0, 0 };
+	
+	//Under constant acceleration what happens 
+	for( int i = 0; i < 1000000; i++ ){
+		integrationScheme.updateBody( body, .000001, 1.0);
+	}
+	auto pos_calc = Vector3f{ 2.5, 0, 0 };
+	std::cout << "integrator calc" << body.position() << "\n";
+	std::cout << "ideal position" << pos_calc << "\n";
+	
+	ASSERT_TRUE( (pos_calc - body.position()).norm() <= pos_calc.norm() * 0.01f );
+	
+}
+
+TEST( integratorTest, eulerIntegration3 ){
+	auto integrationScheme = Euler{};
+	auto body = Body{};
+	//Initial conditions
+	body.position() = Vector3f{ 1, 0, 0 };
+	body.velocity() = Vector3f{ 0, 1, 0 };
+	body.accel() = Vector3f{ 0, 0, 1 };
+	
+	//Under constant acceleration what happens 
+	for( int i = 0; i < 1000000; i++ ){
+		integrationScheme.updateBody( body, .000001, 1.0);
+	}
+	auto pos_calc = Vector3f{ 1, 1, 0.5 };
+	std::cout << "integrator calc" << body.position() << "\n";
+	std::cout << "ideal position" << pos_calc << "\n";
+	
+	ASSERT_TRUE( (pos_calc - body.position()).norm() <= pos_calc.norm() * 0.01f );
+}
+
 TEST( integratorTest, RK4Integration0 ){
 	auto integrationScheme = RungeKutta{};
 	auto body = Body{};
@@ -70,15 +109,55 @@ TEST( integratorTest, RK4Integration1 ){
 	auto integrationScheme = RungeKutta{};
 	auto body = Body{};
 	//Initial conditions
-	body.position() = 
-	body.velocity() = 
-	body.accel()    = 
-
+	body.position() = Vector3f{ 0, 0, 0 };
+	body.velocity() = Vector3f{ 0, 0, 0 };
+	body.accel() = Vector3f{ 1, 0, 0 };
+	
 	//Under constant acceleration what happens 
 	for( int i = 0; i < 1000000; i++ ){
 		integrationScheme.updateBody( body, .000001, 1.0);
 	}
 	auto pos_calc = Vector3f{ 0.5, 0, 0 };
+	std::cout << "integrator calc" << body.position() << "\n";
+	std::cout << "ideal position" << pos_calc << "\n";
+	
+	ASSERT_TRUE( (pos_calc - body.position()).norm() <= pos_calc.norm() * 0.01f );
+	
+}
+
+TEST( integratorTest, RK4Integration2 ){
+	auto integrationScheme = RungeKutta{};
+	auto body = Body{};
+	//Initial conditions
+	body.position() = Vector3f{ 1, 0, 0 };
+	body.velocity() = Vector3f{ 1, 0, 0 };
+	body.accel() = Vector3f{ 1, 0, 0 };
+	
+	//Under constant acceleration what happens 
+	for( int i = 0; i < 1000000; i++ ){
+		integrationScheme.updateBody( body, .000001, 1.0);
+	}
+	auto pos_calc = Vector3f{ 2.5, 0, 0 };
+	std::cout << "integrator calc" << body.position() << "\n";
+	std::cout << "ideal position" << pos_calc << "\n";
+	
+	ASSERT_TRUE( (pos_calc - body.position()).norm() <= pos_calc.norm() * 0.01f );
+	
+}
+
+TEST( integratorTest, RK4Integration3 ){
+	auto integrationScheme = RungeKutta{};
+	auto body = Body{};
+	//Initial conditions
+	body.position() = Vector3f{ 1, 0, 0 };
+	body.velocity() = Vector3f{ 0, 1, 0 };
+	body.accel() = Vector3f{ 0, 0, 1 };
+	
+	//Under constant acceleration what happens 
+	for( int i = 0; i < 1000000; i++ ){
+		integrationScheme.updateBody( body, .000001, 1.0);
+	}
+	auto pos_calc = Vector3f{ 1, 1, 0.5 };
 	std::cout << "integrator calc" << body.position() << "\n";
 	std::cout << "ideal position" << pos_calc << "\n";
 	
@@ -95,7 +174,11 @@ int main() {
 	
 	TEST( integratorTest, eulerIntegration0 );
 	TEST( integratorTest, eulerIntegration1 );
+	TEST( integratorTest, eulerIntegration2 );
+	TEST( integratorTest, eulerIntegration3 );
 	TEST( integratorTest, RK4Integration0 );
 	TEST( integratorTest, RK4Integration1 );
+	TEST( integratorTest, RK4Integration2 );
+	TEST( integratorTest, RK4Integration3 );
 	
 }
