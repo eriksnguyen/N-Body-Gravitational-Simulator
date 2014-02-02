ee85c409793651c709980ae32c768426db59b1c0
Michael Sun <micsun@mit.edu>
2014-02-02 02:00:54 -0500

    updated integration test to include assert

--- test/integratorTest.cpp
+++ test/integratorTest.cpp
@@ -4,6 +4,7 @@
 #include <nbody/rungekutta.h>
 #include <nbody/euler.h>
 #include <gtest/gtest.h>
+#include <assert.h>
 
 TEST( integratorTest, eulerIntegration ){
 	auto integrationScheme = Euler{};
@@ -11,16 +12,18 @@ TEST( integratorTest, eulerIntegration ){
 	//Initial conditions
 	body.position() = 
 	body.velocity() = 
-	body.accel()    = 
+	body.accel() = 
 	
 	//Under constant acceleration what happens 
 	for(int i = 0; i < 1000; i++){
 		integrationScheme.updateBody(body, .000001, 1.0);
 	}
+	float accel_calc = ;
+	CPPUNIT_ASSERT(accel_calc - body.accel() < accel_calc * 0.01f)
 	
 }
 
-TEST( integratorTest, RK4Integration ){
+TEST( integratorTest, RK4Integration ){aj 
 	auto integrationScheme = RungeKutta{};
 	auto body = Body{};
 	//Initial conditions
@@ -32,6 +35,8 @@ TEST( integratorTest, RK4Integration ){
 	for(int i = 0; i < 10000; i++){
 		integrationScheme.updateBody(body, .000001, 1.0);
 	}
+	float accel_calc = ;
+	CPPUNIT_ASSERT(accel_calc - body.accel() < accel_calc * 0.01f)
 	
 	
 }
